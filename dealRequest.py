4c307a4c25321b61e35d241cfe2ead4a91734eba
sun1024 <sunnanbin@gmail.com>
2019-07-18 14:45:26 +0800

    fix raise error

--- dealRequest.py
+++ dealRequest.py
@@ -244,11 +244,11 @@ def dealSecondAuth(data):
     MAC_Key = bytes(getHash(Ku + IDu + Ru))
     msg = encode_data + Ru + Tu + sessionId
     if getHmac(MAC_Key, msg) != MACu:
-        raise Exception('MAC not compare.')
+        raise ValueError('MAC not compare.')
     # 比对信息
     compare_data = decryptData(encode_data, Ku)
     if compare_data != IDu + sessionKey + MACKey:
-        raise Exception('crypt data not compare.')
+        raise ValueError('crypt data not compare.')
     
     # 生成二次认证需要的东西
     new_sessionId = str(getRandom())
